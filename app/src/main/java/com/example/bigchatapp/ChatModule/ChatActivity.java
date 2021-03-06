998bc1567e3fa8d5b253840dab3ce9f42ef07c32
Ali Zahid <ali.zahid050@gmail.com>
2021-03-06 23:02:33 +0500

    commit 107 push ali

--- app/src/main/java/com/example/bigchatapp/ChatModule/ChatActivity.java
+++ app/src/main/java/com/example/bigchatapp/ChatModule/ChatActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.bigchatapp.R;
+import com.example.bigchatapp.databinding.ActivityChatBinding;
 
 public class ChatActivity extends AppCompatActivity {
+
+    ActivityChatBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
+        binding = ActivityChatBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
     }
 }
--- app/src/main/java/com/example/bigchatapp/SplashActivity.java
+++ app/src/main/java/com/example/bigchatapp/SplashActivity.java
@@ -11,6 +11,7 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
 import com.example.bigchatapp.ChatModule.ChatActivity;
+import com.example.bigchatapp.Dashboard.DashboardActivity;
 import com.example.bigchatapp.OnBoarding.OnBoardingActivity;
 import com.example.bigchatapp.databinding.ActivitySplashBinding;
 
@@ -19,19 +20,18 @@ public class SplashActivity extends AppCompatActivity {
     ActivitySplashBinding binding;
     Context context = SplashActivity.this;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySplashBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        Animation myanim = AnimationUtils.loadAnimation(context, R.anim.splash_animation);
-        binding.animateLayout.setAnimation(myanim);
+        Animation myAnim = AnimationUtils.loadAnimation(context, R.anim.splash_animation);
+        binding.animateLayout.setAnimation(myAnim);
 
         new Handler(Looper.getMainLooper()).postDelayed(() -> {
 
-            startActivity(new Intent(context, ChatActivity.class));
+            startActivity(new Intent(context, DashboardActivity.class));
             finish();
 
 
