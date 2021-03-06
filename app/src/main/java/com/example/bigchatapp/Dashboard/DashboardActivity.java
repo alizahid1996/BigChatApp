9b7cf7a0f2af0454f884c15086490a99fe8eb905
zia1122 <ziarehman.cs@gmail.com>
2021-03-06 17:39:53 +0500

    commit 2 Zia

--- app/src/main/java/com/example/bigchatapp/Dashboard/DashboardActivity.java
+++ app/src/main/java/com/example/bigchatapp/Dashboard/DashboardActivity.java
@@ -1,16 +1,27 @@
 package com.example.bigchatapp.Dashboard;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
 
 import android.os.Bundle;
 
+import com.example.bigchatapp.Adapters.FragmentAdapter;
 import com.example.bigchatapp.R;
+import com.example.bigchatapp.databinding.ActivityDashboardBinding;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    ActivityDashboardBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        binding = ActivityDashboardBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.viewPager.setAdapter(new FragmentAdapter(getSupportFragmentManager()));
+        binding.tabLayout.setupWithViewPager(binding.viewPager);
     }
 }
