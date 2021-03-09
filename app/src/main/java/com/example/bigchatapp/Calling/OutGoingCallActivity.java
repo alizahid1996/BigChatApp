c00c71e6607bed6969389ecf70e02b366f382980
zia1122 <ziarehman.cs@gmail.com>
2021-03-09 15:29:15 +0500

    commit 12 Zia

---
+++ app/src/main/java/com/example/bigchatapp/Calling/OutGoingCallActivity.java
@@ -0,0 +1,16 @@
+package com.example.bigchatapp.Calling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bigchatapp.R;
+
+public class OutGoingCallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_out_going_call);
+    }
+}
